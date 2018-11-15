00e096c3a203ee15b7f9d79bf47d9e9acca10852
podshivalov.ilya <podshivalov.ilya@yandex.ru>
2018-11-15 20:11:15 +0300

    Form an apllication code according to task

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Build Status](https://travis-ci.org/ilya-otus/allocator.svg?branch=master)](https://travis-ci.org/ilya-otus/allocator)
-# Helloworld
+# Allocator
 This C++ application with fully configured environment such as:
 - logging (spdlog)
 - tests (boost tests)
--- main.cpp
+++ main.cpp
@@ -6,11 +6,39 @@
 #include "custom_container.h"
 #include "reserve_allocator.h"
 
+const size_t N = 10;
+template <typename T>
+using ReserveNAllocator = ReserveAllocator<T, N>;
 int main(int , char **)
 {
-    auto a = CustomContainer<int, ReserveAllocator<int, 10>>();
+    auto defaultContainerDefaultAllocator = std::map<int, int>();
+    for (size_t i = 0; i < N; ++i) {
+        if (i == 0) {
+            defaultContainerDefaultAllocator[i] = 1;
+        } else {
+            defaultContainerDefaultAllocator[i] = defaultContainerDefaultAllocator[i - 1] * i;
+        }
+    }
+
+    auto defaultContainerCustomAllocator = std::map<int, int, std::less<int>, ReserveNAllocator<std::pair<const int, int> > >();
+    for (size_t i = 0; i < N; ++i) {
+        if (i == 0) {
+            defaultContainerCustomAllocator[i] = 1;
+        } else {
+            defaultContainerCustomAllocator[i] = defaultContainerCustomAllocator[i - 1] * i;
+        }
+        std::cout << i << " " << defaultContainerCustomAllocator[i] << std::endl;
+    }
+
+    auto customContainerDefaultAllocator = CustomContainer<int>();
     for (size_t i = 0; i < 10; ++i) {
-        a.emplace_back(i);
+        customContainerDefaultAllocator.emplace_back(i);
+    }
+
+    auto customContainerCustomAllocator = CustomContainer<int, ReserveNAllocator<int>>();
+    for (size_t i = 0; i < 10; ++i) {
+        customContainerCustomAllocator.emplace_back(i);
+        std::cout << customContainerCustomAllocator[i] << std::endl;
     }
     return 0;
 }
